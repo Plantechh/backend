10c3a2abb260c05a6fde569b6e74f1bd909b2f9f
jeremias <jeremiasprogrammer@gmail.com>
2022-05-28 15:41:43 -0300

    initialize project

---
+++ src/main/java/com/br/cesar/plantech/PlantechApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.cesar.plantech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantechApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantechApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/cesar/plantech/PlantechApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.cesar.plantech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantechApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
