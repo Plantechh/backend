c6a30835aad675e7f10c15767fbdbc912a6aba0a
Pedro Henrique <pedrophaab@hotmail.com>
2022-06-09 23:56:08 -0300

    Added json structure, not proprely correct yet

--- src/main/java/com/br/cesar/plantech/PlantechApiApplication.java
+++ src/main/java/com/br/cesar/plantech/PlantechApiApplication.java
@@ -1,16 +1,20 @@
 package com.br.cesar.plantech;
 
-import java.sql.Connection;
-
+import com.br.cesar.plantech.db.ConnectionFactory;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.br.cesar.plantech.db.ConnectionFactory;
-
-import io.github.cdimascio.dotenv.Dotenv;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
@@ -35,9 +39,28 @@ public class PlantechApiApplication {
 	}
 	
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) throws SQLException {
+		Connection conn = ConnectionFactory.getConnection();
+		assert conn != null;
+		Statement stmt = conn.createStatement();
+		String sql = "SELECT * FROM questions";
+		ResultSet rs = stmt.executeQuery(sql);
+
+		while (rs.next()) {
+			System.out.println(rs.getString("text_is"));
+		}
 		return String.format("Hello %s!", name);
 	}
 	
+	@GetMapping("/Questions")
+	public Map<String, Object> questions() {
+		//json structure
+		Map<String, Object> questions = new LinkedHashMap<>();
+		Map<String, Object> question = new LinkedHashMap<>();
+		ArrayList<Map<String,String>> alternatives = new ArrayList<>();
+		Map<String,String> alternative = new LinkedHashMap<>();
 
+
+		return questions;
+	}
 }
